b0bfb4a30d8c81fd391125585eff145cf1807528
aawezk456 <aawezk456@gmail.com>
2021-03-26 10:43:27 +0530

    Update: config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     SERVICE_ID: "VA90f5177dfbdbfc43f35b9c5dd2242699",
     ACCOUNT_SID: "AC40cd0d34f7428d2f6fbb09757a8bcd91",
-    AUTH_TOKEN: "b5d910dc2bd0fa0f7dbe1e96794fcfde"
+    AUTH_TOKEN: "ee4273f48ae537fd1d3fbdc26af879ee"
 }
